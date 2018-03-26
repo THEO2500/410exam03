1d784f5d2d960e30b62f18e9133b2d19575191e9
THEO2500 <worldpeace871219@gmail.com>
2018-03-26 19:23:12 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a,b,x,y,z;
+	a=in.nextInt();
+	b=in.nextInt();
+	x=a+b;
+	y=a-b;
+	z=a*b;
+	System.out.printf("%d\n%d\n%d\n",x,y,z);
+
+    }
+}
